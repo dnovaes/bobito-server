b242c2ed18981010b4e9c49b95d0ef01ee8671b2
dnovaes <novaesdigo@gmail.com>
2019-05-01 00:26:37 -0300

    fill database dummy with more info

--- config/db_pbMarketing.js
+++ config/db_pbMarketing.js
@@ -6,6 +6,76 @@ const content = [
     id: 1,
     title: "Email Marketing",
     type: 1
+  },
+  {
+    id: 2,
+    title: "Imagem Whatsapp",
+    type: 1
+  },
+  {
+    id: 3,
+    title: "Apresentação PPT",
+    type: 1
+  },
+  {
+    id: 4,
+    title: "Avatar Whatsapp",
+    type: 1
+  },
+  {
+    id: 5,
+    title: "Convite Evento",
+    type: 1
+  },
+  {
+    id: 6,
+    title: "Campanha Outubro Rosa - Folder",
+    type: 1
+  },
+  {
+    id: 7,
+    title: "Campanha Novembro Azul - Folder",
+    type: 1
+  },
+  {
+    id: 8,
+    title: "Testeira",
+    type: 2
+  },
+  {
+    id: 9,
+    title: "Cartão de Visitas",
+    type: 2 
+  },
+  {
+    id: 10,
+    title: "Banner de Lona",
+    type: 2 
+  },
+  {
+    id: 11,
+    title: "Cartazes",
+    type: 2 
+  },
+  {
+    id: 12,
+    title: "Panfletos",
+    type: 2 
+  },
+  {
+    id: 13,
+    title: "Timbrados",
+    type: 2 
+  },
+  {
+    id: 14,
+    title: "Cards",
+    type: 2 
+  },
+  {
+    id: 15,
+    title: "Diagramação(Cartilhas)",
+    type: 2 
   }
 ];
 
